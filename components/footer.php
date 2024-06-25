9a8572a07aba00d0fea2a6233135da1ab862441e
musaalkadzim <122766857+musaalkadzim1@users.noreply.github.com>
2024-06-25 08:14:44 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a>musaalkadzim567@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="https://www.google.com/maps/place/Pasuruan,+Pasuruan+City,+East+Java/@-7.6360195,112.9137217,17z/data=!4m6!3m5!1s0x2dd7c5e1a5fb57df:0xbe8c958dde768096!8m2!3d-7.6469193!4d112.8999225!16zL20vMDdocDYy?entry=ttu">Kota Pasuruan, Indonesia</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">081259654543</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Moh Musa Al Kadzim</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
